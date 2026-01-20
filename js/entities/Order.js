5e2b9d423f326bcdc291469496c59594ba9aa7c8
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-20 14:21:17 +0100

    Add getters and setters

--- js/entities/Order.js
+++ js/entities/Order.js
@@ -13,4 +13,29 @@ export class Order {
     this.confirmed = confirmed;
     this.confirmedDate = confirmedDate;
   }
+
+  // ===== GETTERS SETTERS =====
+  get id() { return this.#id; }
+  set id(value) {
+    if (!Number.isInteger(value) || value <= 0) throw new Error("Invalid ID");
+    this.#id = value;
+  }
+
+  get orderDate() { return this.#orderDate; }
+  set orderDate(value) {
+    if (!(value instanceof Date)) throw new Error("Order date must be a Date object");
+    this.#orderDate = value;
+  }
+
+  get confirmed() { return this.#confirmed; }
+  set confirmed(value) {
+    if (typeof value !== "boolean") throw new Error("Confirmed must be boolean");
+    this.#confirmed = value;
+  }
+
+  get confirmedDate() { return this.#confirmedDate; }
+  set confirmedDate(value) {
+    if (value !== null && !(value instanceof Date)) throw new Error("Confirmed date must be a Date or null");
+    this.#confirmedDate = value;
+  }
 }
