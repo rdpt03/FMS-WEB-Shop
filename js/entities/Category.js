c37d7974762a1de41d6df8aa8fd25039645c0cd8
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-20 15:06:03 +0100

    refactor tabulation

--- js/entities/Category.js
+++ js/entities/Category.js
@@ -2,26 +2,28 @@
 // Class representing a Category in the shop system
 
 export class Category {
-  #id;
-  #name;
+    #id;
+    #name;
 
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
-    this.stock = stock;
-    this.price = price;
-    this.totalOrdered = totalOrdered;
-  }
+    // ===== Constructor =====
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+        this.totalOrdered = totalOrdered;
+    }
 
-  get id() { return this.#id; }
-  set id(value) {
-    if (!Number.isInteger(value) || value <= 0) throw new Error("Invalid ID");
-    this.#id = value;
-  }
+    // ==== Getters Setters
+    get id() { return this.#id; }
+    set id(value) {
+        if (!Number.isInteger(value) || value <= 0) throw new Error("Invalid ID");
+        this.#id = value;
+    }
 
-  get name() { return this.#name; }
-  set name(value) {
-    if (!value || value.length < 1) throw new Error("Name is required");
-    this.#name = value;
-  }
+    get name() { return this.#name; }
+    set name(value) {
+        if (!value || value.length < 1) throw new Error("Name is required");
+        this.#name = value;
+    }
 }
--- js/entities/SubCategory.js
+++ js/entities/SubCategory.js
@@ -1,27 +1,29 @@
 // article.js
 // Class representing a SubCategory in the shop system
 
-export class SubCategory {
-  #id;
-  #name;
+export class Category {
+    #id;
+    #name;
 
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
-    this.stock = stock;
-    this.price = price;
-    this.totalOrdered = totalOrdered;
-  }
+    // ===== Constructor =====
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+        this.totalOrdered = totalOrdered;
+    }
 
-  get id() { return this.#id; }
-  set id(value) {
-    if (!Number.isInteger(value) || value <= 0) throw new Error("Invalid ID");
-    this.#id = value;
-  }
+    // ==== Getters Setters
+    get id() { return this.#id; }
+    set id(value) {
+        if (!Number.isInteger(value) || value <= 0) throw new Error("Invalid ID");
+        this.#id = value;
+    }
 
-  get name() { return this.#name; }
-  set name(value) {
-    if (!value || value.length < 1) throw new Error("Name is required");
-    this.#name = value;
-  }
+    get name() { return this.#name; }
+    set name(value) {
+        if (!value || value.length < 1) throw new Error("Name is required");
+        this.#name = value;
+    }
 }
