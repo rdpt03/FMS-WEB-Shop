001c6e553fe7656c56b67f44a620f7b71a39e69e
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-20 15:01:18 +0100

    a add category and subcategory

---
+++ js/entities/Category.js
@@ -0,0 +1,27 @@
+// article.js
+// Class representing a Category in the shop system
+
+export class Category {
+  #id;
+  #name;
+
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    this.stock = stock;
+    this.price = price;
+    this.totalOrdered = totalOrdered;
+  }
+
+  get id() { return this.#id; }
+  set id(value) {
+    if (!Number.isInteger(value) || value <= 0) throw new Error("Invalid ID");
+    this.#id = value;
+  }
+
+  get name() { return this.#name; }
+  set name(value) {
+    if (!value || value.length < 1) throw new Error("Name is required");
+    this.#name = value;
+  }
+}
