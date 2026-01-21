c1660de987fbcdc14380a27bd48072488e0046aa
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-21 16:15:30 +0100

    add all functions related to get articles
    add getSubCaterories() function

--- js/localStorage/loadData.js
+++ js/localStorage/loadData.js
@@ -1,3 +1,4 @@
+import { Article } from '../entities/Article.js';
 import { SubCategory } from '../entities/SubCategory.js';
 import { Category } from './../entities/Category.js';
 
@@ -22,7 +23,7 @@ export function getCategories(){
     return listOfCategories
 }
 
-export function getSubCategoriesByCategory(category){
+function getSubCategoriesByCategory(category){
     const listOfSubCategoriesForTheCategory = [];
     //get from json
     const storedSubCategories = JSON.parse(localStorage.getItem("subCategories"));
@@ -32,9 +33,51 @@ export function getSubCategoriesByCategory(category){
         //check if the categories are the same
         if (SubCategJson.category == category.id){
             //if yes add to a list
-            listOfSubCategoriesForTheCategory.push(new SubCategory(SubCategJson.id, SubCategJson.name, SubCategJson.category));
+            listOfSubCategoriesForTheCategory.push(new SubCategory(SubCategJson.id, SubCategJson.name, category));
         }
     })
     //return
     return listOfSubCategoriesForTheCategory;
+}
+
+export function getSubCategories(){
+    const subCategoriesList = [];
+    getCategories().forEach(category => {
+        category.subCategories.forEach(subCateg => {
+            subCategoriesList.push(subCateg);
+        })
+    });
+
+    return subCategoriesList;
+}
+
+// ============================================== ARTICLES ============================================== 
+export function getArticles(){
+    const listOfArticles = [];
+    const listOfCategories = getSubCategories();
+    //get from json
+    const storedArticles = JSON.parse(localStorage.getItem("articles"));
+    
+    //get categories 
+    storedArticles.forEach(storedArticle => {
+        const subCategory = listOfCategories.find(subCateg => subCateg.id === storedArticle.subCategory)
+        listOfArticles.push(new Article(storedArticle.id, storedArticle.name ,storedArticle.stock, storedArticle.price, subCategory, storedArticle.totalOrdered ))
+    });
+    
+    //foreach
+    console.log(listOfArticles);
+    return listOfArticles
+}
+
+export function getArticleByCategoryId(categoryId){
+    if(typeof categoryId !== 'number' || isNaN(categoryId)){
+        throw new Error("category id must be a number");
+    }
+    return getArticles().filter(article => article.subCategory.category.id === categoryId);
+}
+export function getArticleBySubCategoryId(categoryId){
+    if(typeof categoryId !== 'number' || isNaN(categoryId)){
+        throw new Error("category id must be a number");
+    }
+    return getArticles().filter(article => article.subCategory.id === categoryId);
 }
