33543319a5e2149a0bf1a33dad4ca0efcd219bf9
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-21 16:14:07 +0100

    reorder code and add articles

--- js/localStorage/createValues.js
+++ js/localStorage/createValues.js
@@ -1,3 +1,4 @@
+import { Article } from '../entities/Article.js';
 import { Category } from '../entities/Category.js';
 import { SubCategory } from '../entities/SubCategory.js';
 
@@ -6,22 +7,33 @@ const categories = [
   new Category(1, "Voitures"),
   new Category(2, "Pieces detachés"),
 ];
+localStorage.setItem('categories', JSON.stringify(categories));
+console.log("Categories saved to LocalStorage:", categories);
 
 const subCategories = [
-    new SubCategory(1,"Sportif",categories[0]).toJSON(),
-    new SubCategory(2,"SUV",categories[0]).toJSON(),
-    new SubCategory(3,"Liquides",categories[1]).toJSON(),
-    new SubCategory(4,"Moteurs",categories[1]).toJSON(),
-
+    new SubCategory(1,"Sportif",categories[0]).toJSON(), //0
+    new SubCategory(2,"SUV",categories[0]).toJSON(), //1 
+    new SubCategory(3,"Liquides",categories[1]).toJSON(), //2
+    new SubCategory(4,"Moteurs",categories[1]).toJSON(), //3
 ]
-
-
-// Step 2 & 3: save directly to LocalStorage
-// JSON.stringify automatically calls article.toJSON()
-localStorage.setItem('categories', JSON.stringify(categories));
 localStorage.setItem('subCategories', JSON.stringify(subCategories))
-
-console.log("Categories saved to LocalStorage:", categories);
 console.log("Sub saved to LocalStorage:", subCategories);
 
-localStorage.removeItem('shopData')
+const articles = [
+    new Article(1,"Ferrari", 2, 400000, subCategories[0]).toJSON(),
+    new Article(2,"Liquide Clignotant", 100, 8.99, subCategories[2]).toJSON(),
+    new Article(3,"Moteur puretech", 10, 1.99, subCategories[3] ).toJSON(),
+    new Article(4,"Peugeot 206cc", 10, 2000, subCategories[0]).toJSON(),
+    new Article(5,"Toyota", 4, 40000, subCategories[1]).toJSON(),
+    new Article(6,"Huile moteur", 62, 3, subCategories[2]).toJSON(),
+]
+localStorage.setItem('articles', JSON.stringify(articles))
+console.log("articles saved to LocalStorage:", articles);
+
+
+
+
+
+
+
+
