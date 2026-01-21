b794f7d0c42fb0a6b5aa38aec0358bec48e61cc9
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-21 14:13:49 +0100

    add subcaterogy creation into localStorage

--- js/localStorage/createValues.js
+++ js/localStorage/createValues.js
@@ -1,4 +1,5 @@
 import { Category } from '../entities/Category.js';
+import { SubCategory } from '../entities/SubCategory.js';
 
 // Step 1: create multiple Article instances
 const categories = [
@@ -6,8 +7,21 @@ const categories = [
   new Category(2, "Pieces detachés"),
 ];
 
+const subCategories = [
+    new SubCategory(1,"Sportif",categories[0]).toJSON(),
+    new SubCategory(2,"SUV",categories[0]).toJSON(),
+    new SubCategory(3,"Liquides",categories[1]).toJSON(),
+    new SubCategory(4,"Moteurs",categories[1]).toJSON(),
+
+]
+
+
 // Step 2 & 3: save directly to LocalStorage
 // JSON.stringify automatically calls article.toJSON()
 localStorage.setItem('categories', JSON.stringify(categories));
+localStorage.setItem('subCategories', JSON.stringify(subCategories))
 
 console.log("Categories saved to LocalStorage:", categories);
+console.log("Sub saved to LocalStorage:", subCategories);
+
+localStorage.removeItem('shopData')
