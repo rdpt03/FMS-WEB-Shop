59c1d914e45d605a02cbe38fac93d33ac1e1bb4f
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-21 08:51:36 +0100

    Add sidebar import

--- js/view-loader/importNavBarAndFooter.js
+++ js/view-loader/importNavBarAndFooter.js
@@ -1,10 +1,17 @@
 // Seleciona o elemento onde o navbar será inserido
 const navbarContainer = document.getElementById('navbar');
-console.log("docaralho");
+const sidebarContainer = document.getElementById('sidebar');
 // Carrega o HTML do navbar
 fetch('./../components/navbar.html')
   .then(response => response.text())
   .then(html => {
     navbarContainer.innerHTML = html;
+  })
+  .catch(err => console.error('Erro ao carregar navbar:', err));
+
+fetch('./../components/sidebar.html')
+  .then(response => response.text())
+  .then(html => {
+    sidebarContainer.innerHTML = html;
   })
   .catch(err => console.error('Erro ao carregar navbar:', err));
