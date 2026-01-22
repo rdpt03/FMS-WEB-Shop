c27535be5cd82f56b3565b86a279ba5fa435f1db
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-22 23:36:54 +0100

    add idle system

---
+++ js/manageTimeout.js
@@ -0,0 +1,25 @@
+import { emptyCart } from "./localStorage/manageCart.js";
+import { goHome } from "./router.js";
+
+let idleTimer;
+
+function resetIdleTimer() {
+    console.log('start')
+    clearTimeout(idleTimer);
+
+    idleTimer = setTimeout(() => {
+        goHome();
+        emptyCart();
+
+        // Restart timer to keep checking inactivity continuously
+        resetIdleTimer();
+    }, 5*1000);
+}
+
+// Event listeners
+["mousemove", "keydown", "mousedown", "touchstart", "scroll"].forEach(event =>
+    window.addEventListener(event, resetIdleTimer)
+);
+
+// Start timer on page load
+resetIdleTimer();
