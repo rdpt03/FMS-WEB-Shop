633aafc491ab5b7e8bb0cd6a0b8f7d2b5c821be4
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-22 10:30:28 +0100

    Refactor file to adapt a SPA logic

--- js/view-loader/load-articles.js
+++ js/view-loader/load-articles.js
@@ -1,90 +1,61 @@
-import { getArticleByCategoryId, getArticleBySubCategoryId, getArticles } from "./../localStorage/loadData.js"
+import { getArticles } from "./../localStorage/loadData.js"
 
-let articles = [];
-//-----------OLD
-//get url params
-const params = new URLSearchParams(window.location.search);
-
-//get if is category or subcategory
-const filterBy = params.get('filter_by');
-
-//get the id of it
-const category_id = Number(params.get('category_id')); 
-
-//if category given
-if(filterBy == 'category'){
-    articles = getArticleByCategoryId(category_id);
-}
-
-//if sub category
-else if(filterBy == 'sub_category'){
-    articles = getArticleBySubCategoryId(category_id);
-}
-
-//else
-else{
-    articles = getArticles();
-}
-//------------new to replace old
-articles = getArticles();
+//get the articles
+let articles = getArticles();
 
 //get container
 const container = document.getElementById("articles-container");
 
-//for each article create the card
-articles.forEach(article => {
-    //create div
-    const col = document.createElement("div");
-    //appli bootsrap classes
-    col.className = "col-3 col-md-4 d-flex justify-content-center p-1";
+//render the articles
+renderArticles(articles)
 
-    //prepair the html code
-    col.innerHTML = `
+//function to render it 
+export function renderArticles(list) {
+    container.innerHTML = ""; // clean actual articles
+
+    list.forEach(article => {
+        const col = document.createElement("div");
+        col.className = "col-3 col-md-4 d-flex justify-content-center p-1";
+
+        col.innerHTML = `
         <div class="card" style="width: 300px; height: 400px; background-color:#bbbbbb;">
-            <img src="${article.img}" 
-                class="card-img-top" 
-                alt="${article.name}" 
-                style="height: 200px; object-fit: contain;">
-
+            <img src="${article.img}" class="card-img-top" alt="${article.name}" style="height: 200px; object-fit: contain;">
             <div class="card-body d-flex flex-column">
                 <h5 class="card-title">${article.name}</h5>
                 <p class="card-text flex-grow-1"></p>
-                
                 <div class="row">
-                    <!-- Left column -->
                     <div class="col-md-6">
                         <p class="card-text mb-0">${article.subCategory.name}</p>
                         <p class="card-text mb-0">${article.subCategory.category.name}</p>
                     </div>
-
-                    <!-- Right Column -->
                     <div class="col-md-6 d-flex justify-content-center align-items-center">
                         <p class="card-text mb-0">${article.price}€</p>
                     </div>
                 </div>
-                <div class="row">
-                    <!-- Left column -->
+                <div class="row mt-2">
                     <div class="col-md-6">
-                        <a href="${article.link}"
-                        class="btn btn-primary mt-2 w-100 d-flex justify-content-center align-items-center"
-                        style="height: 48px;">
+                        <a href="${article.link}" class="btn btn-primary w-100 d-flex justify-content-center align-items-center" style="height: 48px;">
                             Details
                         </a>
                     </div>
-
-                    <!-- Right Column -->
                     <div class="col-md-6">
-                        <a href="${article.link}"
-                        class="btn btn-primary mt-2 w-100 d-flex justify-content-center align-items-center"
-                        style="height: 48px;">
+                        <a href="#" class="btn btn-primary w-100 d-flex justify-content-center align-items-center add-to-cart" style="height: 48px;">
                             Ajouter au Panier
                         </a>
                     </div>
                 </div>
             </div>
         </div>
-    `;
+        `;
+        container.appendChild(col);
+    });
 
-    //apply
-    container.appendChild(col);
-});
+    // add event shopping cart
+    const cartButtons = container.querySelectorAll(".add-to-cart");
+    cartButtons.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            console.log("added to card:", list[index].name);
+            // add logic here soon
+        });
+    });
+}
